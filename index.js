0fb655c52d0f1f36331234b0204f5130b98f3685
Parker Franzi <Parker.franzi@gmail.com>
2019-07-03 18:35:02 -0700

    added functionality

--- index.js
+++ index.js
@@ -2,27 +2,34 @@
 
 function getGithubRepo(handle) {
   fetch('https://api.github.com/users/'+ handle +'/repos')
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .then(response => {
+        console.log(response);
+        if (response.ok) {
+            console.log(response);
+            return response.json();
+        }
+        throw new Error(response.message);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => errorResults());
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
-  if (responseJson.status === 'success') {
-    $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-   )
-  }
-  else {
-    $('.results-img').replaceWith(`<p class="results-img">${responseJson.message}</p>`)
-  
+  $('#results-list').empty();
+  for (let i=0; i<responseJson.length; i++) {
+      $('#results-list').append(
+          `<li><p>Name: ${responseJson[i].name}</p>
+          <p>Link: ${responseJson[i].html_url}</p>`
+      )
   }
   //display the results section
   $('.results').removeClass('hidden');
 }
-
+function errorResults() {
+    $('.results').removeClass('hidden');
+    $('#results-list').html('<p class="error">Something went wrong please try again</p>')
+}
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
